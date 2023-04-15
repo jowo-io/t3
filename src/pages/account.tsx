71f2efc5fe9188cf0aeee29f67daeaffc123f456
JoWo <pureth@googlemail.com>
2023-04-15 13:14:54 +0200

    adding signedUrl endpoint

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -5,8 +5,10 @@ import { api } from "@/utils/api";
 const Home: NextPage = () => {
   const hello = api.account.hello.useQuery({ text: "from account tRPC" });
   const account = api.account.getAccount.useQuery();
-  // const user = account.data[0];
+  const signedUrl = api.account.getSignedAvatarUrl.useQuery();
+
   console.log("account:", account.data);
+  console.log("signedUrl:", signedUrl.data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="flex flex-col items-center gap-2">
--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 import { eq } from "drizzle-orm/expressions";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 
 import {
   createTRPCRouter,
@@ -9,6 +11,17 @@ import {
 
 import { users } from "@/db/auth";
 
+const bucketName = "plunda";
+
+const s3 = new S3Client({
+  endpoint: `https://${process.env.STORAGE_ENDPOINT}`,
+  region: process.env.STORAGE_REGION,
+  credentials: {
+    accessKeyId: process.env.STORAGE_ACCESS_KEY as string,
+    secretAccessKey: process.env.STORAGE_SECRET_KEY as string,
+  },
+});
+
 export const accountRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
@@ -17,6 +30,19 @@ export const accountRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
+
+  getSignedAvatarUrl: publicProcedure.query(async ({ ctx }) => {
+    const userId = ctx.session?.user?.id;
+    if (!userId) return null;
+    const pathName = `avatars/${userId}.webp`;
+    const input = {
+      Bucket: bucketName,
+      Key: pathName,
+    };
+    const command = new GetObjectCommand(input);
+    const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+    return url;
+  }),
 
   getAccount: publicProcedure.query(({ ctx }) => {
     const email = ctx.session?.user?.email;
