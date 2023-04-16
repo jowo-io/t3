e24969943998289bfba03dcc25dabc9eea15ac42
JoWo <pureth@googlemail.com>
2023-04-16 22:08:02 +0200

    adding empty ssl config

--- src/server/db.ts
+++ src/server/db.ts
@@ -10,6 +10,7 @@ const poolConnection =
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {},
   });
 
 if (process.env.NODE_ENV !== "production")
