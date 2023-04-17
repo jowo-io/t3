aefcfc61b156b8a37c2907e81f375e09ffd94c06
JoWo <pureth@googlemail.com>
2023-04-17 08:33:25 +0200

    updating ssl option config

--- src/server/db.ts
+++ src/server/db.ts
@@ -10,7 +10,7 @@ const poolConnection =
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {},
+    ...(process.env.NODE_ENV === "production" ? { ssl: {} } : {}),
   });
 
 if (process.env.NODE_ENV !== "production")
