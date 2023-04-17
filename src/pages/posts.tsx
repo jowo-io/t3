1b75957e3849c216d1efea618422a1d98500c070
JoWo <pureth@googlemail.com>
2023-04-17 09:11:42 +0200

    adding basic posts page and moving auth button to component

---
+++ src/pages/posts.tsx
@@ -0,0 +1,56 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
+import { eq, and } from "drizzle-orm/expressions";
+
+import { authOptions } from "@/server/auth";
+import { db } from "@/server/db";
+import { posts } from "@/db/schema";
+
+export const getServerSideProps = async ({
+  req,
+  res,
+}: GetServerSidePropsContext) => {
+  const session = await getServerSession(req, res, authOptions);
+  const userId = session?.user?.id;
+  if (!session || !userId) return { props: { feed: [] } };
+
+  const feed = await db
+    .select()
+    .from(posts)
+    .where(and(eq(posts.user_id, userId), eq(posts.published, true)));
+
+  console.log("feed", userId, feed);
+  const out: {
+    props: { feed: typeof feed };
+  } = {
+    props: {
+      feed: JSON.parse(JSON.stringify(feed)), // work around to serialize date object
+    },
+  };
+  return out;
+};
+
+export default function PostsPage({
+  feed,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      {session && (
+        <div>
+          <h1>posts</h1>
+          <hr />
+          {feed.map((post) => (
+            <div key={post.id}>
+              <h3 className="text-2xl text-blue-300">{post.title}</h3>
+              <p>{post.text}</p>
+              <hr />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
