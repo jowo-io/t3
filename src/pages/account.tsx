539c0c46f8c0ec6929b1c704462481a4c1ff4241
JoWo <pureth@googlemail.com>
2023-04-23 08:39:07 +0200

    small style changes to account page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -78,70 +78,76 @@ const AccountPage: NextPage = () => {
     <main className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4">
       <Header />
 
-      <div className="flex w-full max-w-xs flex-col gap-2 py-2">
-        <h2 className="mb-5 text-3xl font-bold text-white">Your account</h2>
-
-        <div>
-          <h3 className="text-lg font-bold text-white">Avatar image</h3>
-          {account?.image && (
-            <img src={env.NEXT_PUBLIC_STORAGE_URL + account?.image} />
-          )}
-          <input
-            type="file"
-            accept="image/png, image/jpeg, image/gif, image/webp"
-            onChange={uploadPhoto}
-            disabled={isLoading}
-            className="m-0 rounded-sm border-black bg-slate-500 p-2 font-bold text-white"
-          />
-        </div>
-
-        <hr className="my-8 h-px w-full border-0 bg-white" />
-        <form
-          onSubmit={methods.handleSubmit(async (values) => {
-            await updateAccount.mutateAsync(values);
-          })}
-          className="space-y-2"
-        >
-          <h3 className="text-lg font-bold text-white">Account details</h3>
+      <div className="flex flex-col items-center gap-2">
+        <div className="flex w-full max-w-xs flex-col gap-2 py-2">
+          <h2 className="mb-5 text-3xl font-bold text-white">Your account</h2>
 
           <div>
-            <label>
-              <span className="text-white">Email</span>
-              <br />
-              <input
-                id="email"
-                type="text"
-                className="w-full border p-1"
-                value={account?.email || ""}
-                disabled
+            <h3 className="mb-2 text-lg font-bold text-white">Avatar image</h3>
+            {account?.image && (
+              <img
+                alt="Avatar image"
+                className="mb-2"
+                src={env.NEXT_PUBLIC_STORAGE_URL + account?.image}
               />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              <span className="text-white">Name</span>
-              <br />
-              <input
-                {...methods.register("name")}
-                className="w-full border p-1"
-              />
-            </label>
-            {methods.formState.errors.name?.message && (
-              <p className="text-red-700">
-                {methods.formState.errors.name?.message}
-              </p>
             )}
+            <input
+              type="file"
+              accept="image/png, image/jpeg, image/gif, image/webp"
+              onChange={uploadPhoto}
+              disabled={isLoading}
+              className="m-0 rounded-sm border-black bg-slate-500 p-2 font-bold text-white"
+            />
           </div>
 
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="m-0 rounded-sm border-black bg-slate-500 p-2 font-bold text-white"
+          <hr className="my-8 h-px w-full border-0 bg-white" />
+          <form
+            onSubmit={methods.handleSubmit(async (values) => {
+              await updateAccount.mutateAsync(values);
+            })}
+            className="space-y-2"
           >
-            {isLoading ? "Loading" : "Submit"}
-          </button>
-        </form>
+            <h3 className="text-lg font-bold text-white">Account details</h3>
+
+            <div>
+              <label>
+                <span className="text-white">Email</span>
+                <br />
+                <input
+                  id="email"
+                  type="text"
+                  className="w-full border p-1"
+                  value={account?.email || ""}
+                  disabled
+                />
+              </label>
+            </div>
+
+            <div>
+              <label>
+                <span className="text-white">Name</span>
+                <br />
+                <input
+                  {...methods.register("name")}
+                  className="w-full border p-1"
+                />
+              </label>
+              {methods.formState.errors.name?.message && (
+                <p className="text-red-700">
+                  {methods.formState.errors.name?.message}
+                </p>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="m-0 rounded-sm border-black bg-slate-500 p-2 font-bold text-white"
+            >
+              {isLoading ? "Loading" : "Submit"}
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
