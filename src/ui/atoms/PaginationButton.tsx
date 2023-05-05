d5d08acc1f67563ba54ec7a602fb717c17289610
JoWo <pureth@googlemail.com>
2023-05-05 12:07:07 +0100

    tweaking pagination ui layout

--- src/ui/atoms/PaginationButton.tsx
+++ src/ui/atoms/PaginationButton.tsx
@@ -1,34 +1,20 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes } from "react";
 
-const paginationButton = cva(
-  [
-    "relative",
-    "button",
-    "inline-flex",
-    "justify-center",
-    "items-center",
-    "ring-1",
-    "ring-inset",
-    "ring-gray-300",
-    "px-2",
-    "py-2",
-  ],
-  {
-    variants: {
-      position: {
-        left: ["rounded-l-md"],
-        center: ["px-4"],
-        right: ["rounded-r-md"],
-      },
-      intent: {
-        enable: ["bg-white", "text-gray-900"],
-        disable: ["bg-white", "text-gray-400", "cursor-default"],
-        highlight: ["bg-indigo-600", "text-white", "cursor-default"],
-      },
+const paginationButton = cva(["px-2", "py-2"], {
+  variants: {
+    position: {
+      left: ["rounded-l-md"],
+      center: ["px-4"],
+      right: ["rounded-r-md"],
     },
-  }
-);
+    intent: {
+      enable: ["bg-white", "text-gray-900"],
+      disable: ["bg-white", "text-gray-400", "cursor-default"],
+      highlight: ["bg-indigo-600", "text-white", "cursor-default"],
+    },
+  },
+});
 
 export interface PaginationButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
--- src/ui/molecules/Pagination.tsx
+++ src/ui/molecules/Pagination.tsx
@@ -22,61 +22,53 @@ export default function Pagination({
   const isLast = page === pages - 1;
 
   return (
-    <div className="px-4 py-3">
-      <div className="pb-3 text-center">
-        <p className="text-sm text-white">
-          Showing{" "}
-          <span className="font-medium">{page * resultsPerPage + 1}</span> to{" "}
-          <span className="font-medium">
-            {Math.min(count, page * resultsPerPage + resultsPerPage)}
-          </span>{" "}
-          of <span className="font-medium">{count}</span> results
-        </p>
-      </div>
-      <div className="flex flex-1 items-center justify-between">
-        <div>
-          <nav
-            className="isolate inline-flex -space-x-px rounded-md bg-white shadow-sm "
-            aria-label="Pagination"
+    <div className="flex w-full flex-col justify-center px-4 py-3">
+      <p className="pb-3 text-center text-sm text-white">
+        Showing <span className="font-medium">{page * resultsPerPage + 1}</span>{" "}
+        to{" "}
+        <span className="font-medium">
+          {Math.min(count, page * resultsPerPage + resultsPerPage)}
+        </span>{" "}
+        of <span className="font-medium">{count}</span> results
+      </p>
+      {pages > 1 && (
+        <nav
+          className="grid grid-cols-10 justify-items-center overflow-hidden rounded-md bg-white shadow-sm"
+          aria-label="Pagination"
+        >
+          <PaginationButton
+            position="left"
+            intent={isFirst ? "disable" : "enable"}
+            onClick={() => onChange(page - 1)}
           >
-            <PaginationButton
-              position="left"
-              intent={isFirst ? "disable" : "enable"}
-              onClick={() => onChange(page - 1)}
-            >
-              <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </PaginationButton>
+            <span className="sr-only">Previous</span>
+            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+          </PaginationButton>
 
-            {repeat((i) => {
-              return (
-                <PaginationButton
-                  aria-current="page"
-                  key={i + 1}
-                  position="center"
-                  intent={page === i ? "highlight" : "enable"}
-                  onClick={() => onChange(i)}
-                >
-                  {i + 1}
-                </PaginationButton>
-              );
-            }, pages)}
+          {repeat((i) => {
+            return (
+              <PaginationButton
+                aria-current="page"
+                key={i + 1}
+                position="center"
+                intent={page === i ? "highlight" : "enable"}
+                onClick={() => onChange(i)}
+              >
+                {i + 1}
+              </PaginationButton>
+            );
+          }, pages)}
 
-            {/* <PaginationButton position="center" intent="disable">
-              {"..."}
-            </PaginationButton> */}
-
-            <PaginationButton
-              position="right"
-              intent={isLast ? "disable" : "enable"}
-              onClick={() => onChange(page + 1)}
-            >
-              <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </PaginationButton>
-          </nav>
-        </div>
-      </div>
+          <PaginationButton
+            position="right"
+            intent={isLast ? "disable" : "enable"}
+            onClick={() => onChange(page + 1)}
+          >
+            <span className="sr-only">Next</span>
+            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+          </PaginationButton>
+        </nav>
+      )}
     </div>
   );
 }
